c31348aec9e1d8881274894756e18c4e3b4a6985
Nisha kumari <nishak96610@gmail.com>
2025-08-11 22:39:24 +0530

    Add files via upload

---
+++ Positive_negative_zero.js
@@ -0,0 +1,11 @@
+num = Number(prompt("enter the number: "));
+
+if (num >= 1){
+    console.log(num, " is Positive number");
+}
+else if (num < 0){
+    console.log(num, " is negative number");
+}
+else{
+    console.log("zero");
+}
